082267408bb87db69b17e5e9bece3f43333a8af4
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-22 20:57:15 -0400

    ad postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     content: [
         './public/index.html'
-    ]
-}
+    ],
+    theme: {
+        screens: {
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          xl: '1440px',
+        }
+    }
+} 
