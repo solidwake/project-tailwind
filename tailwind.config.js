cc8f65f7080b174062447a6f62686520a014f021
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-23 09:07:52 -0400

    add custom plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
     mode: 'jit',
     content: [
         './public/index.html',
         './public/**/*.{html,js}'
+    ],
+    plugins: [
+        plugin(function ({addUtilities, addVariant}) {
+            const newUtilities = {
+                '.scale-1': {
+                    transform: 'scale(1)'
+                },
+                '.rotate-1': {
+                    transform: 'rotate(1)'
+                }
+            }
+            addUtilities(newUtilities)
+            addVariant('hocus', ['&:hover', '&:focus'])
+        })
     ],
     theme: {
         screens: {
